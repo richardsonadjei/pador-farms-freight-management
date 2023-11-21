e776b66763241ecd3939e9e3116c7b5e7d83bc85
richardsonadjei <adjei.kwadjo@gmail.com>
2023-11-21 13:29:29 +0000

    Created The pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import CreateAccount from './pages/CreateAccount';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div className="text-primary">
-      App
-    </div>
-  );
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/create-account' element={<CreateAccount />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
+  </BrowserRouter>
+  )
 }
